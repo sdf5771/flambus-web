607a4618ea925d2a679a393e94b296118c2cb35c
SeobisBack <seobisback@gmail.com>
2023-10-01 16:07:56 +0900

    feat: KakaoMap.tsx update

--- src/components/Pages/KakaoMap.tsx
+++ src/components/Pages/KakaoMap.tsx
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import { Map, useKakaoLoader } from 'react-kakao-maps-sdk';
+import { Map, useKakaoLoader, MapMarker } from 'react-kakao-maps-sdk';
 
 const API_KEY = process.env.REACT_APP_KAKAO_MAP_API_KEY
 
@@ -7,6 +7,14 @@ function KakaoMap(){
     const [deviceWidth, setDeviceWidth] = useState<number | null>(null);
     const [deviceHeight, setDeviceHeight] = useState<number | null>(null);
     const [loading, error] = useKakaoLoader({appkey: API_KEY ? API_KEY : ''});
+
+    const testLocations = [
+        { title: "선지해장국 우주점", location: {lat: 33.5563, lng: 126.79581} },
+        { title: "떡볶이 우주점", location: {lat: 34.5563, lng: 128.79581} },
+        { title: "뼈해장국 우주점", location: {lat: 35.5563, lng: 130.79581} },
+        { title: "미라클미라클 빙그르르 우주점", location: {lat: 36.5563, lng: 122.79581} },
+    ]
+    
     useEffect(() => {
         setDeviceWidth(window.innerWidth);
         setDeviceHeight(window.innerHeight);
@@ -20,6 +28,23 @@ function KakaoMap(){
                     style={{ width: `${deviceWidth}px`, height: `${deviceHeight}px` }} // 지도 크기
                     level={3}                                   // 지도 확대 레벨
                 >
+                    {
+                        testLocations.map((data, index) => {
+                            return (
+                                <MapMarker
+                                    key={`${data.title}-${data.location}`}
+                                    position={data.location}
+                                    image={{
+                                        src: "",
+                                        size: {width: 24, height: 24}
+                                    }}
+                                    title={data.title}
+                                >
+                                
+                                </MapMarker>
+                            )
+                        })
+                    }
                 </Map>
             : <h1>loading...</h1>}
         </div>
